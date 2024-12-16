5380e77b3ac6d55aef6e404b9e7f248a23b2bd63
conde.j <conde.j@proimpo.com>
2024-12-16 07:47:13 -0500

    Update generate_bank_file

--- generate_bank_file/generate_bank_file/__manifest__.py
+++ generate_bank_file/generate_bank_file/__manifest__.py
@@ -1,17 +1,15 @@
 {
     'name': "Generar Archivos Bancos Colombia",
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
     'description': """
         Generar archivos planos para bancos Colombia
     """,
     'author': 'DOXOO S.A.S.',
+    'company': 'DOXOO S.A.S.',
+    'maintainer': 'DOXOO S.A.S.',
     'website': "http://www.doxoo.co",
-    'category': 'Accounting',
-    "version": "17.0.1.0.0",
     'depends': ['account_payment'],
-    'license': 'LGPL-3',
-    'images': [
-    ],
-    'installable': True,
     'data': [
         'security/security.xml',
         'views/res_bank_parameter_view.xml',
@@ -20,6 +18,9 @@
         'security/ir.model.access.csv',
         
     ],
-    'demo': [
-    ],
+    'license': 'LGPL-3',
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
 }
