2e0766f6cd94ae2758aeba96ce41fb2cd4ef3456
conde.j <conde.j@proimpo.com>
2025-11-13 20:11:04 -0500

    Fix: manifest

--- biometric_api/__manifest__.py
+++ biometric_api/__manifest__.py
@@ -1,15 +1,18 @@
 {
-    "name": "Biometric API Integration Full",
-    "version": "1.1",
-    "summary": "API completa para integracion de dispositivos biometricos",
-    "depends": [
+    'name': "Biometric API Integration Full",
+    'version': "1.1",
+    'author': 'DEV S.A.S',
+    'summary': "API completa para integracion de dispositivos biometricos",
+    'category': 'Generic Modules/Human Resources',
+    'depends': [
         "base",
         "hr",
         "hr_attendance"
     ],
-    "data": [
+    'description': "Modulo biometrico",
+    'data': [
         "security/ir.model.access.csv"
     ],
-    "installable": true,
-    "application": false
+    "installable": True,
+    "application": False
 }
