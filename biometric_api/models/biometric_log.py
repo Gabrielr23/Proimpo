81725d69a39bc553bc60d72df4bf6e3bfef9a8ae
conde.j <conde.j@proimpo.com>
2025-11-21 08:09:51 -0500

    Remove Files:: Biometric api and Theoretical vs Attended Time Analysis

--- biometric_api/models/biometric_log.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class BiometricLog(models.Model):
-    _name = 'biometric.log'
-    _description = 'Log de datos recibidos del Biométrico'
-    _order = 'create_date desc'
-
-    payload = fields.Json(string="Payload")
-    create_date = fields.Datetime(string="Fecha de recepción", readonly=True)
