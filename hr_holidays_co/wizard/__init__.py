f9e9ae65a265c80f75cfc090fa394ca6170b58a8
conde.j <conde.j@proimpo.com>
2026-03-10 11:52:13 -0500

    Remove hr_holidays_co

--- hr_holidays_co/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import create_novedades_wizard
-from . import create_novedades_consolidated_wizard
