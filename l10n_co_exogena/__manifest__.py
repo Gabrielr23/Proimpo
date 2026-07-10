a441f1210318e13835124be80bb8f14b473b5ac7
Conde JM <conde.j@proimpo.com>
2026-07-10 08:02:34 -0500

    Update l10n_co_exogena II

--- l10n_co_exogena/__manifest__.py
+++ l10n_co_exogena/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Información Exógena DIAN (Colombia)',
-    'version': '18.0.1.2.0',
+    'version': '18.0.1.3.0',
     'category': 'Accounting/Localizations/Reporting',
     'summary': 'Genera los formatos de información exógena (medios magnéticos) '
                'de la DIAN a partir de la contabilidad de Odoo.',
