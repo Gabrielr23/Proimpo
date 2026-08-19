e8d3ecad370aa8104149df301c7aa35ea6f5bda4
conde.j <conde.j@proimpo.com>
2026-08-19 13:22:07 -0500

    Install proimpo_nomina_certificado

--- proimpo_nomina_certificado/__manifest__.py
+++ proimpo_nomina_certificado/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "PROIMPO SAS",
     'license': "LGPL-3",
     'category': "Human Resources",
-    'version': "18.0.1.3.1",
+    'version': "18.0.1.3.2",
     'depends': ['l10n_co_hr_payroll_enterprise', 'proimpo_nomina_liquidacion'],
     'data': [
         'security/ir.model.access.csv',
