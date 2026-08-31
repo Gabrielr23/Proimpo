e9f56327210f88a727a8ad0f534ba5c6311e2dc1
conde.j <conde.j@proimpo.com>
2026-08-31 08:13:14 -0500

    Fix: add limit=1 to postar_search

--- l10n_co_edi_jorels/models/res_partner.py
+++ l10n_co_edi_jorels/models/res_partner.py
@@ -151,14 +151,14 @@ class ResPartner(models.Model):
         for rec in self:
             if rec.zip and rec.country_id and rec.country_id.code == 'CO':
                 postal_obj = rec.env['l10n_co_edi_jorels.postal']
-                postal_search = postal_obj.sudo().search([('name', '=', rec.zip)])
+                postal_search = postal_obj.sudo().search([('name', '=', rec.zip)],limit=1)
                 if postal_search:
                     rec.postal_id = postal_search[0].id
                     rec.postal_department_id = rec.env['l10n_co_edi_jorels.departments'].sudo().search(
-                        [('code', '=', rec.postal_id.department_id.code)]
+                        [('code', '=', rec.postal_id.department_id.code)],limit=1
                     )[0].id
                     rec.postal_municipality_id = rec.env['l10n_co_edi_jorels.municipalities'].sudo().search(
-                        [('code', '=', rec.postal_id.municipality_id.code)]
+                        [('code', '=', rec.postal_id.municipality_id.code)],limit=1
                     )[0].id
             else:
                 rec.postal_id = None
