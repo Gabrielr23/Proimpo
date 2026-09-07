b08477f1f1d1efca955447f3afcbcd3c5114f22d
conde.j <conde.j@proimpo.com>
2026-09-07 14:16:57 -0500

    Fix:: l10n_co_hr_payroll_enterprise

--- l10n_co_hr_payroll_enterprise/__manifest__.py
+++ l10n_co_hr_payroll_enterprise/__manifest__.py
@@ -27,7 +27,7 @@
     'author': 'PROIMPO SAS',
     'license': 'LGPL-3',
     'category': 'Human Resources',
-    'version': '19.0.24.01.310120',
+    'version': '19.0.24.01.310121',
     'website': 'https://www.proimpo.com',
     'images': ['static/images/main_screenshot.png'],
     'support': 'info@proimpo.com',
