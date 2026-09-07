97d0c153a6dae2c5dadbc8be707411a8fb3b55fc
conde.j <conde.j@proimpo.com>
2026-09-07 12:25:49 -0500

    Fix:: modules motor

--- l10n_co_hr_payroll_enterprise/models/res_users.py
+++ l10n_co_hr_payroll_enterprise/models/res_users.py
@@ -38,7 +38,7 @@ HR_WRITABLE_FIELDS = [
 
 
 class User(models.Model):
-    _inherit = ['res.users']
+    _inherit = 'res.users'  # Odoo 19: _inherit en lista sin _name crea un modelo nuevo
 
     private_postal_id = fields.Many2one(related='employee_id.private_postal_id', string="Postal", readonly=False,
                                         related_sudo=False)
