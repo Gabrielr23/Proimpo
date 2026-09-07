32443e1fee0b17c00ad4a9b5b9174393e56921a1
conde.j <conde.j@proimpo.com>
2026-09-07 12:53:37 -0500

    Fix:: modules motor

--- l10n_co_hr_payroll_enterprise/__manifest__.py
+++ l10n_co_hr_payroll_enterprise/__manifest__.py
@@ -27,7 +27,7 @@
     'author': 'PROIMPO SAS',
     'license': 'LGPL-3',
     'category': 'Human Resources',
-    'version': '19.0.24.01.310118',
+    'version': '19.0.24.01.310119',
     'website': 'https://www.proimpo.com',
     'images': ['static/images/main_screenshot.png'],
     'support': 'info@proimpo.com',
